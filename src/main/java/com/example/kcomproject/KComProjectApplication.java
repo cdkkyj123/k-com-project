2da4740cde2f61e9690a8c2eb0e9bd09573db60c
cdkkyj123 <cdkkyj123@gmail.com>
2026-05-01 15:52:14 +0900

    first commit

---
+++ src/main/java/com/example/kcomproject/KComProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.kcomproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KComProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KComProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kcomproject/KComProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kcomproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KComProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
